3fe70a6d1105f1c2256470e81ec000b6a2bc14ce
Nao YONASHIRO <owan.orisano@gmail.com>
2019-11-06 22:57:20 +0900

    test: improve coverage

--- damm_test.go
+++ damm_test.go
@@ -13,45 +13,55 @@ func TestIsValid(t *testing.T) {
 		"9876",
 		"100101010102030",
 	}
-	a := damm.Digit
-	for _, tc := range ts {
-		b := []byte(tc)
-		d, err := damm.CheckSymbol(a, b)
-		if err != nil {
-			t.Errorf("failed to calculate check digit: %v", err)
+	as := []damm.Alphabet{
+		damm.Digit,
+		damm.LowerHex,
+		damm.UpperHex,
+	}
+	for _, a := range as {
+		for _, tc := range ts {
+			testIsValid(t, a, []byte(tc))
+		}
+	}
+}
+
+func testIsValid(t *testing.T, a damm.Alphabet, b []byte) {
+	t.Helper()
+	d, err := damm.CheckSymbol(a, b)
+	if err != nil {
+		t.Errorf("failed to calculate check digit: %v", err)
+		return
+	}
+	b = append(b, d)
+	for i := 0; i < len(b)-1; i++ {
+		if b[i] == b[i+1] {
 			continue
 		}
-		b = append(b, d)
-		for i := 0; i < len(b)-1; i++ {
-			if b[i] == b[i+1] {
-				continue
-			}
+		var bb []byte
+		bb = append(bb, b...)
+		bb[i], bb[i+1] = bb[i+1], bb[i]
+		ok, err := damm.IsValid(a, bb)
+		if err != nil {
+			t.Errorf("failed to check check digit: %v", err)
+			continue
+		}
+		if ok {
+			t.Errorf("unexpected result of validation, expected to invalid: %q", string(bb))
+		}
+	}
+	for i := 0; i < len(b); i++ {
+		for ord := uint8(0); ord < a.Size(); ord++ {
+			c := a.Chr(ord)
 			var bb []byte
 			bb = append(bb, b...)
-			bb[i], bb[i+1] = bb[i+1], bb[i]
+			bb[i] = c
 			ok, err := damm.IsValid(a, bb)
 			if err != nil {
 				t.Errorf("failed to check check digit: %v", err)
 				continue
 			}
-			if ok {
+			if (c == b[i]) != ok {
 				t.Errorf("unexpected result of validation, expected to invalid: %q", string(bb))
-			}
-		}
-		for i := 0; i < len(b); i++ {
-			for ord := uint8(0); ord < a.Size(); ord++ {
-				c := a.Chr(ord)
-				var bb []byte
-				bb = append(bb, b...)
-				bb[i] = c
-				ok, err := damm.IsValid(a, bb)
-				if err != nil {
-					t.Errorf("failed to check check digit: %v", err)
-					continue
-				}
-				if (c == b[i]) != ok {
-					t.Errorf("unexpected result of validation, expected to invalid: %q", string(bb))
-				}
 			}
 		}
 	}
